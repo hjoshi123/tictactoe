26b37bbf4048f41d37fe89a1444c3bf5310f919a
Hemant Joshi <HemantJ@Hemant.local>
2017-05-15 11:44:56 +0530

    Autoplay added

--- app/src/main/java/com/example/hemantj/ticatactoe/MainActivity.java
+++ app/src/main/java/com/example/hemantj/ticatactoe/MainActivity.java
@@ -4,17 +4,23 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     int activePlayer = 1; //1 for X and 2 for Y
     ArrayList<Integer> player1 = new ArrayList<>();
     ArrayList<Integer> player2 = new ArrayList<>();
-    private static boolean gameOver = false;
+    ArrayList<Integer> emptyPlace = new ArrayList<>();
+
+    private int choice;
+    private static final String TAG = "MAIN";
+    private boolean gameOver = false;
     private static int draw = 0;
     Button btn1;    Button btn2;
     Button btn3;    Button btn4;
@@ -26,6 +32,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Bundle bundle = getIntent().getExtras();
+        choice = bundle.getInt("play");
 
         btn1 = (Button) findViewById(R.id.btn1);    btn2 = (Button) findViewById(R.id.btn2);
         btn3 = (Button) findViewById(R.id.btn3);    btn4 = (Button) findViewById(R.id.btn4);
@@ -38,7 +46,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btn5.setOnClickListener(this);  btn6.setOnClickListener(this);
         btn7.setOnClickListener(this);  btn8.setOnClickListener(this);
         btn9.setOnClickListener(this);
-
     }
 
     @Override
@@ -88,17 +95,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     void gamePlay(int cellId,Button btn){
-        if(activePlayer == 1){
-            btn.setText("X");
-            btn.setBackgroundColor(Color.argb(1,50,100,200));
-            player1.add(cellId);
-            activePlayer = 2;
+        Log.d(TAG,String.valueOf(choice));
+
+        if(choice == 1){
+            if(activePlayer == 1){
+                btn.setText("X");
+                btn.setBackgroundColor(Color.argb(1,50,100,200));
+                player1.add(cellId);
+                activePlayer = 2;
+            }
+            else if(activePlayer == 2){
+                btn.setText("O");
+                btn.setBackgroundColor(Color.argb(1,100,200,256));
+                player2.add(cellId);
+                activePlayer = 1;
+            }
         }
-        else if(activePlayer == 2){
-            btn.setText("O");
-            btn.setBackgroundColor(Color.argb(1,100,200,256));
-            player2.add(cellId);
-            activePlayer = 1;
+        else if(choice == 2){
+            if(activePlayer == 1){
+                btn.setText("X");
+                btn.setBackgroundColor(Color.argb(1,50,100,200));
+                player1.add(cellId);
+                activePlayer = 2;
+                autoPlay();
+            }
+            else if(activePlayer == 2){
+                btn.setText("O");
+                btn.setBackgroundColor(Color.argb(1,100,200,256));
+                player2.add(cellId);
+                activePlayer = 1;
+            }
         }
 
         btn.setEnabled(false);
@@ -148,5 +174,50 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Toast.makeText(this,"Player 2 is the winner",Toast.LENGTH_SHORT).show();
             gameOver = true;
         }
+    }
+
+    void autoPlay(){
+        for(int i=1;i<=9;i++){
+            if(!(player1.contains(i) || player2.contains(i)))
+                emptyPlace.add(i);
+        }
+        Random r = new Random();
+        int randIndex = r.nextInt(emptyPlace.size());
+        int cellId = emptyPlace.get(randIndex);
+
+        Button btnSelected;
+        switch (cellId){
+            case 1:
+                btnSelected = btn1;
+                break;
+            case 2:
+                btnSelected = btn2;
+                break;
+            case 3:
+                btnSelected = btn3;
+                break;
+            case 4:
+                btnSelected = btn4;
+                break;
+            case 5:
+                btnSelected = btn5;
+                break;
+            case 6:
+                btnSelected = btn6;
+                break;
+            case 7:
+                btnSelected = btn7;
+                break;
+            case 8:
+                btnSelected = btn8;
+                break;
+            case 9:
+                btnSelected = btn9;
+                break;
+            default:
+                btnSelected = btn1;
+        }
+
+        gamePlay(cellId,btnSelected);
     }
 }
